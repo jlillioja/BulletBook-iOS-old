44f8bb5970c63315dfb4acf0d2da32c8a809a83e
jlillioja <jlillioja@gmail.com>
2018-01-04 17:46:45 -0600

    Dynamic note list

--- BulletBook/ViewController.swift
+++ BulletBook/ViewController.swift
@@ -9,17 +9,52 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let noteStack = UIStackView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        let addButton = UIButton()
+        addButton.translatesAutoresizingMaskIntoConstraints = false
+        addButton.setTitle("+", for: .normal)
+        addButton.setTitleColor(.white, for: .normal)
+        addButton.backgroundColor = .black
+        addButton.addTarget(self, action: #selector(addNote), for: .touchUpInside)
+        view.addSubview(addButton)
+        let addButtonConstraints = [
+            addButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10),
+            addButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor)
+        ]
+        
+        noteStack.translatesAutoresizingMaskIntoConstraints = false
+        noteStack.axis = .vertical
+        view.addSubview(noteStack)
+        let noteStackConstraints = [
+            noteStack.topAnchor.constraint(equalTo: addButton.bottomAnchor),
+            noteStack.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            noteStack.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+        ]
+        
+        let firstNote = UITextField()
+        firstNote.translatesAutoresizingMaskIntoConstraints = false
+        firstNote.textColor = .black
+        firstNote.text = "Your first note!"
+        noteStack.addArrangedSubview(firstNote)
+        
+        NSLayoutConstraint.activate(noteStackConstraints + addButtonConstraints)
     }
-
-
+    
+    @objc private func addNote() {
+        let newNote = UITextField()
+        newNote.textColor = .black
+        newNote.placeholder = "New Note"
+        noteStack.addArrangedSubview(newNote)
+    }
 }
 
